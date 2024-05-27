da13211933b6c8815885f8a6560a98e6d0a7fd1a
raliporace <raffaele.liporace@eng.it>
2024-05-27 12:30:32 +0200

    aggiunta angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForComponent } from './for/for.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ForComponent
+    ForComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatIconModule,MatSidenavModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
